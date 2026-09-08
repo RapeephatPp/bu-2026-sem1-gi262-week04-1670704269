f0529247057ca65fe7dc0a3bd997c1d974edab19
RapeephatPp <rapeephat.prat@bumail.net>
2026-09-08 17:13:48 +0700

    Lecture

--- Assets/Scripts/Workspace/Assignment/Lecture.cs
+++ Assets/Scripts/Workspace/Assignment/Lecture.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
-
+using System.Collections
+    ;
 namespace Assignment
 {
     public class Lecture : MonoBehaviour
@@ -8,9 +9,9 @@ namespace Assignment
         public void Start()
         {
             // LCT01_SyntaxList();
-            // LCT02_SyntaxLinkedList();
+            LCT02_SyntaxLinkedList();
             // LCT03_SyntaxHashTable();
-            // LCT04_SyntaxDictionary();
+            LCT04_SyntaxDictionary();
         }
 
         #region Lecture
@@ -22,17 +23,82 @@ namespace Assignment
 
         public void LCT02_SyntaxLinkedList()
         {
-            throw new System.NotImplementedException();
+            LinkedList<string> linkedList = new LinkedList<string>();
+            linkedList.AddLast("Node 1");
+            linkedList.AddLast("Node 2");
+            
+            linkedList.AddFirst("Node 0");
+            
+            LinkedListNode<string> node1 = linkedList.Find("Node 1");
+            Debug.Log(node1.Value);
+            Debug.Log(node1.Next.Value);
+            Debug.Log(node1.Previous.Value);
+
+            var firstNode = linkedList.First;
+            var lastNode = linkedList.Last;
+            Debug.Log(firstNode.Previous);
+            Debug.Log(lastNode.Next);
+            
+            linkedList.AddAfter(node1, "Node 1.5");
+            linkedList.AddBefore(node1, "Node 0.5");
+            
+            linkedList.RemoveFirst();
+            linkedList.RemoveLast();
+            linkedList.Remove("Node 1.5");
+            
+            linkedList.Clear();
+            Debug.Log("=====================");
+            foreach (var item in linkedList)
+            {
+                Debug.Log(item);
+            }
         }
 
         public void LCT03_SyntaxHashTable()
         {
-            throw new System.NotImplementedException();
+            Hashtable table = new Hashtable();
+            table.Add("Potion", 5);
+            table.Add(5, "Potion");
+
+            foreach (var item in table)
+            {
+                Debug.Log($"Item {item}");
+            }
         }
 
         public void LCT04_SyntaxDictionary()
         {
-            throw new System.NotImplementedException();
+            Dictionary<string, int> inv = new Dictionary<string, int>();
+            
+            inv.Add("Potion", 5);
+            inv.Add("banana", 1);
+            inv.Add("Apple", 10);
+            
+            inv["Apple"] = 0;
+
+            inv["Apple1"] = 1;
+            
+            int potion = inv["Potion"];
+            Debug.Log("Potion: " + potion);
+
+            /*
+            int apple2 = inv["Apple2"];
+            Debug.Log("Apple2: " + apple2);*/
+            
+            bool hasPotion = inv.ContainsKey("Potion");
+            Debug.Log("hasPotion: " + hasPotion);
+            
+            inv.Remove("Banana");
+
+            foreach (KeyValuePair<string, int> kvp in inv)
+            {
+                var key = kvp.Key;
+                var value = kvp.Value;
+                Debug.Log($"{key} => {value}");
+                /*inv.Add("mm", 1);*/
+            }
+            
+            inv.Clear();
         }
 
         #endregion
--- Assets/Workshop/Student/Scripts/collectAbleItem.cs
+++ Assets/Workshop/Student/Scripts/collectAbleItem.cs
@@ -8,7 +8,9 @@ namespace Solution
         {
             Debug.Log("Item: " + Name + " has been picked up.");
             // ทำลายไอเท็มออกจากฉาก
-
+            Destroy(gameObject);
+            mapGenerator.player.inventory.AddItem(Name, 1);
+            
             return true;
         }
     }
